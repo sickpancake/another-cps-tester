d641568b5ed7bb7a62dda0c6a8db19738b666259
Lawrence Wang <Larry@LarryMacBook-Air.local>
2025-04-06 11:28:25 +0800

    add the timer with callback function

--- src/backend.py
+++ src/backend.py
@@ -1,13 +1,17 @@
 """Backend code for Another-CPS-Tester"""
+from threading import Timer
 from time import time
+from typing import Callable
 
 class BackendCPSTester:
     """Manages all backend code for Another-CPS-Tester"""
-    def __init__(self):
+    def __init__(self, callback_function: Callable[[], None]):
         self.current_clicks = 0
         self.config_test_duration = 5 #seconds
         self.start_time = time()
         self.end_time = self.start_time + self.config_test_duration
+
+        self.timer = Timer(0.01, callback_function)
 
     def add_click(self):
         """Adds a click"""
