8e12a5241e0c1d949e88871eb72fd945619e733a
Lawrence Wang <Larry@LarryMacBook-Air.local>
2025-04-12 20:47:43 +0800

    adds more test for timer

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -72,3 +72,32 @@ class TestCPSTester(unittest.TestCase):
         self.cps_tester.start()
         sleep(self.cps_tester.config_test_duration + 0.01)
         self.assertTrue(self.cps_tester.is_time_up())
+
+    def test_callback_function_called_periodically(self):
+        """Tests if the callback function is called multiple times during the test duration"""
+        call_count = 0
+
+        def cb(_):
+            nonlocal call_count
+            call_count += 1
+
+        self.cps_tester = BackendCPSTester(cb)
+        self.cps_tester.start()
+        sleep(1)
+        self.assertGreater(call_count, 1)
+
+    def test_click_count_persists_after_timer_ends(self):
+        """Tests if the click_count variable still exists after the timer ends"""
+        self.cps_tester.start()
+        self.cps_tester.add_click()
+        self.cps_tester.add_click()
+        sleep(self.cps_tester.config_test_duration+0.01)
+        self.assertEqual(self.cps_tester.current_clicks, 2)
+    
+    def test_custom_test_duration(self):
+        """Tests if a custom test time work correctly"""
+        self.cps_tester.config_test_duration = 3
+        self.cps_tester.start()
+        self.assertFalse(self.cps_tester.is_time_up())
+        sleep(3.1)
+        self.assertTrue(self.cps_tester.is_time_up())
