70a0688e2abc0a1ee4b4927c9b0f3acee477cc0c
sickpancake <71183834+sickpancake@users.noreply.github.com>
2025-04-27 11:55:34 +0800

    Add instant calculation and final calculation (#8)
    Co-authored-by: Lawrence Wang <Larry@LarryMacBook-Air.local>

--- src/backend.py
+++ src/backend.py
@@ -3,13 +3,26 @@ from threading import Thread
 from time import time, sleep
 from typing import Callable
 
+class InstantResult:
+    """A class with all the information for the instant results"""
+    def __init__(self):
+        self.current_clicks = -1
+        self.current_cps = -1
+        self.time_left = -1
+
+class FinalResult:
+    """A class with all the information for the final results"""
+    def __init__(self):
+        self.total_clicks = -1
+        self.total_cps = -1
+
 class BackendCPSTester:
     """Manages all backend code for Another-CPS-Tester"""
     def __init__(self, callback_function_external: Callable[[bool], None]):
         self.current_clicks = 0
-        self.config_test_duration = 5 #seconds
-        self.start_time = time()
-        self.end_time = self.start_time + self.config_test_duration
+        self.config_test_duration = 5.0 #seconds
+        self.start_time = 0
+        self.end_time = 0
 
         self.callback_function_external = callback_function_external
         self.background_thread = Thread(target=self.background_loop, daemon=True)
@@ -34,3 +47,26 @@ class BackendCPSTester:
         while not self.is_time_up():
             self.callback_function_external(self.is_time_up())
             sleep(0.01)
+
+    def instant_calculate(self) -> InstantResult:
+        """Calculates the instant results"""
+        dummy = InstantResult()
+        t = time()
+        dummy.current_clicks = self.current_clicks
+        dummy.current_cps = round(self.current_clicks/(t-self.start_time), 2)
+        itu = self.is_time_up()
+        if itu:
+            dummy.time_left = 0
+        else:
+            dummy.time_left = round(self.end_time-t, 2) #seconds
+
+        return dummy
+
+
+    def final_calculate(self) -> FinalResult:
+        """Calculates the final results"""
+        dummy = FinalResult()
+        if self.end_time != 0 and self.is_time_up():
+            dummy.total_clicks = self.current_clicks
+            dummy.total_cps = self.current_clicks/5
+        return dummy
