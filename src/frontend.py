708729ba059709bb94f94ee4321645a783392702
Lawrence Wang <Larry@LarryMacBook-Air.local>
2025-05-10 21:18:32 +0800

    WIP frontend

--- src/frontend.py
+++ src/frontend.py
@@ -4,16 +4,37 @@ import flet as ft
 def FrontendCPSTester(page: ft.Page):
     """Flet code for Another-CPS-Tester"""
 
-    def close_app(e):
+    def close_app(_):
         page.window.destroy()
 
     close_button = ft.IconButton(icon="close", on_click=close_app, tooltip="close window")
 
+    click_button = ft.ElevatedButton(text="Click Me!" , width=100, height=50)
+
+    page.vertical_alignment = "spaceBetween"
     page.add(
-        ft.Row(
-            [close_button],
-            alignment="end"
-        ),
+        ft.Column(
+            [
+                ft.Row(
+                    [close_button],
+                    alignment="end"
+                ),
+                ft.Row(
+                    [click_button],
+                    alignment="center"
+                ),
+                ft.Row(
+                    [
+                        ft.Text(
+                            value="Brought to you by: sickpancake",
+                            color="gray"
+                        )
+                    ],
+                    alignment="center"
+                )
+            ],
+            alignment="spaceBetween"
+        )
     )
     page.update()
 
