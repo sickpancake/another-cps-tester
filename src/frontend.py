cddbc0f6e8ea3b53b44ccf96f0908e3194ef927b
Lawrence Wang <Larry@LarryMacBook-Air.local>
2025-05-11 10:53:19 +0800

    add more frontend stuff

--- src/frontend.py
+++ src/frontend.py
@@ -7,35 +7,54 @@ def FrontendCPSTester(page: ft.Page):
     def close_app(_):
         page.window.destroy()
 
-    close_button = ft.IconButton(icon="close", on_click=close_app, tooltip="close window")
+    def open_final_result(_):
+        total_clicks = 0
+        total_cps = 0
 
-    click_button = ft.ElevatedButton(text="Click Me!" , width=100, height=50)
+        final_result_dialogue = ft.AlertDialog(
+            title=ft.Text(value="CPS Test Results"),
+            content=ft.Column(
+                [
+                    ft.Text(value=f"Total Clicks: {total_clicks}"),
+                    ft.Text(value=f"Clicks Per Second (CPS): {total_cps}"),
+                    ft.Text(value="Thank you for using Another-CPS-Tester!"),
+                ],
+                tight=True
+            ),
+            actions=[
+                ft.IconButton(icon="close", on_click=lambda _: page.close(final_result_dialogue), tooltip="Close Results")
+            ]
+        )
+        page.open(page.open(final_result_dialogue))
+        page.update()
+
+    close_button = ft.IconButton(icon="close", on_click=close_app, tooltip="Close APP")
+
+    click_button = ft.ElevatedButton(text="Click Me!" , width=200, height=100)
 
     page.vertical_alignment = "spaceBetween"
     page.add(
-        ft.Column(
+        ft.Row(
+            [close_button],
+            alignment="end"
+        ),
+        ft.Row(
+            [click_button],
+            alignment="center"
+        ),
+        ft.Row(
             [
-                ft.Row(
-                    [close_button],
-                    alignment="end"
-                ),
-                ft.Row(
-                    [click_button],
-                    alignment="center"
-                ),
-                ft.Row(
-                    [
-                        ft.Text(
-                            value="Brought to you by: sickpancake",
-                            color="gray"
-                        )
-                    ],
-                    alignment="center"
+                ft.Text(
+                    value="sickpancake",
                 )
             ],
-            alignment="spaceBetween"
+            alignment="center"
         )
+        
     )
+
+    page.title = "Another-CPS-Tester"
+
     page.update()
 
 def openCPSTester():
